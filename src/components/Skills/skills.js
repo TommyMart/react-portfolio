49e65efaa7122d57e26176d7280a4128eedabb2a
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2025-01-22 20:15:30 +1030

    skills update

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -12,20 +12,16 @@ const Skills = () => {
       </span>
       <p className="skillsDesc">
         Tom is an enthusiastic and versatile junior full-stack developer with a
-        deep passion for the rapidly evolving tech landscape, including
-        advancements in AI and emerging digital innovations. With a foundation
+        deep passion for the rapidly evolving tech landscape. With a foundation
         in marketing, visual design, and full-stack development, Tom seamlessly
         combines creativity with technical proficiency, crafting user-centered
-        applications that are as functional as they are visually compelling. His
-        academic background and hands-on experience enable him to integrate
-        design and functionality while navigating the fast-paced challenges of
-        today’s technology. Known for his keen attention to detail, logical
-        thinking, and dedication to continuous learning, Tom is motivated by the
-        exciting possibilities within the tech industry, from AI-driven
-        innovations to the growing emphasis on sustainability. He aims to build
-        solutions that not only enhance user experiences but also positively
-        impact society and the environment, driving both personal and
-        professional growth.
+        applications that are as functional as they are visually compelling.
+        Known for his keen attention to detail, logical thinking, and dedication
+        to continuous learning, Tom is motivated by the exciting possibilities
+        within the tech industry, from AI-driven innovations to the growing
+        emphasis on sustainability. He aims to build solutions that not only
+        enhance user experiences but also positively impact society and the
+        environment.
       </p>
       <br />
 
@@ -47,10 +43,10 @@ const Skills = () => {
           <div className="skillBarText">
             <h2>Back-End Development</h2>
             <p className="skillsDesc">
-              I have a deep passion and solid knowledge in backend technologies
-              such as Node.js, Express, Flask, PostgreSQL, and MongoDB. This
-              combination enables me to build not only aesthetically pleasing
-              but also robust, scalable, and high-performing applications.
+              I have a solid knowledge of backend technologies such as Node.js,
+              Express, Flask, PostgreSQL, and MongoDB. This combination enables
+              me to build not only aesthetically pleasing but also robust,
+              scalable, and high-performing applications.
             </p>
           </div>
         </div>
@@ -61,10 +57,7 @@ const Skills = () => {
             <p className="skillsDesc">
               I have a strong foundation in UI/UX design, with a focus on
               crafting intuitive, user-friendly interfaces that balance
-              aesthetics and functionality. My approach ensures that every
-              design element, from layout to interaction, enhances the overall
-              user experience, leading to visually engaging and seamless
-              applications.
+              aesthetics and functionality.
             </p>
           </div>
         </div>
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -58,9 +58,7 @@ const Projects = () => {
               offering intuitive data visualisation through charts and graphs.
               Developed as my capstone project at Coder Academy, it utilises
               TypeScript, React, ShadCN, Express, Node.js, and MongoDB Atlas to
-              deliver a seamless and efficient experience. Designed with
-              simplicity and functionality in mind, SurveyBuddy helps users
-              easily create surveys and analyse results with clarity.
+              deliver a seamless and efficient experience.
             </p>
             <a
               href="https://surveybuddy.tech/"
@@ -78,11 +76,10 @@ const Projects = () => {
             <img src={api} alt="" className="projectBarImg"></img>
 
             <p className="projectDesc">
-              One of my notable projects, completed as part of an assignment for
-              which I received a high distinction, was my introduction to
-              backend development and working with APIs. I built a RESTful API
-              web server using Python, Flask, SQLAlchemy, and PostgreSQL, where
-              user information is authorised via JWT and securely hashed using
+              One of my notable projects, was my introduction to backend
+              development and working with APIs. I built a RESTful API web
+              server using Python, Flask, SQLAlchemy, and PostgreSQL, where user
+              information is authorised via JWT and securely hashed using
               Bcrypt. This project helped me develop a deep understanding of API
               architecture and secure user authentication systems.
             </p>
@@ -100,16 +97,11 @@ const Projects = () => {
             <h2 className="projectTitle">Terminal Application</h2>
             <img src={pythonApp} alt="" className="projectBarImg"></img>
             <p className="projectDesc">
-              As part of an assignment for which I received a high distinction,
               I built a fully functional quiz night terminal application using
               Python, Bash scripting, and rich styling for an enhanced user
               experience. This project required creating an intuitive
               command-line interface, handling user input, and managing quiz
-              logic and scoring. The use of Python allowed for efficient data
-              processing, while Bash scripting helped automate various tasks
-              within the terminal environment. This project significantly
-              enhanced my understanding of backend logic and provided valuable
-              experience in building interactive console applications.
+              logic and scoring.
             </p>
             <a
               href="https://github.com/TommyMart/QuizNight_Terminal_Application"
@@ -128,9 +120,7 @@ const Projects = () => {
               This portfolio website, developed using React, showcases my
               expertise in modern web development. It leverages powerful tools
               such as React Scroll, React Router, EmailJS, and hooks to create a
-              seamless and interactive user experience. The minimalist design
-              not only highlights my projects but also embodies a contemporary
-              aesthetic, ensuring a clean and engaging interface.
+              seamless and interactive user experience.
             </p>
             <a
               href="https://github.com/TommyMart/react-portfolio"
@@ -148,12 +138,9 @@ const Projects = () => {
             <p className="projectDesc">
               The OpenAI Electronic Music Tutorial App is an innovative platform
               designed to help users enhance their music production skills in
-              electronic genres. By leveraging the power of OpenAI's language
-              model, the app provides personalised guidance dynamically tailored
-              to each user's experience level, age, and preferred production
-              methods. Users can engage in dynamic conversations with the AI,
-              asking questions and receiving real-time feedback and tutorials,
-              all while exploring various aspects of electronic music creation.{" "}
+              electronic genres. The app provides personalised guidance
+              dynamically tailored to each user's experience level, age, and
+              preferred production methods.{" "}
             </p>
             <a
               href="https://github.com/TommyMart/music-chat-bot"
@@ -170,15 +157,11 @@ const Projects = () => {
             <h2 className="projectTitle">Pokemon API Game</h2>
             <img src={pokemon} alt="" className="projectBarImg"></img>
             <p className="projectDesc">
-              Dive into the exciting world of my Pokemon Browser Game, built
-              with JavaScript and the Pokemon API. In this interactive web
-              application, players can catch Pokemon that appear on the screen
-              using their Pokemon ball cursor, aiming for a high score. The game
-              features a competitive scoreboard to track your best catches and
-              allows users to switch between different themes for a personalised
-              experience. This project highlights my skills in API integration,
-              event handling, and creating engaging user interfaces, all while
-              celebrating the fun of Pokemon-catching.
+              An interactive web application, where players can catch Pokemon
+              that appear on the screen using their Pokemon ball cursor, aiming
+              for a high score. The game features a competitive scoreboard to
+              track your best catches and allows users to switch between
+              different themes for a personalised experience.
             </p>
             <a
               href="https://catch-a-pokemon.netlify.app"
@@ -196,10 +179,7 @@ const Projects = () => {
             <p className="projectDesc">
               The dice rolling app is a dynamic and interactive tool designed to
               simulate the rolling of dice, offering users a visually appealing
-              and engaging experience. Built using JavaScript, HTML, and CSS,
-              the app allows users to input a desired dice size (ranging from 1
-              to 20 sides) and roll the dice to generate a random outcome. It
-              also includes a fun "Count to 100" game.
+              and engaging experience.
             </p>
             <a
               href="https://github.com/TommyMart/Dice_Rolling_App/"
